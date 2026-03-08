97e4d45cfbf33bd833a949c101eadf5fc88f1993
Tarekul <tarekul03.muslim@gmail.com>
2026-03-09 01:34:59 +0530

    added sales hist

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/v1';
+const BASE_URL = 'https://medix-backend-1-0.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: BASE_URL,
